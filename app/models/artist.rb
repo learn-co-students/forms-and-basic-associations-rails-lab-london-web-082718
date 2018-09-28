17102cb767ec92802bacb5ecf96a424e9861bd6e
github username <github email address>
2018-09-28 12:15:08 +0100

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,25 @@
 class Artist < ActiveRecord::Base
-  # add associations here
+
+  has_many :songs
+  has_many :genres, through: :songs
+  # has_many :notes, through: :songs
+
+
 end
+
+
+
+# The base models, controllers, and seed data have been provided for you,
+#
+# but the associations have not been wired up.
+#
+# Artists and genres have many songs.
+# Songs belong to both and have many notes.
+# Notes belong to songs.
+
+
+# 
+# Artist  --< Song  >-- Genre
+#              ^
+#              |
+#             Note
